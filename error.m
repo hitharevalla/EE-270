044fbe5f47de8011d3b80a2071b0e2ef60ef50b9
hitharevalla <43685030+hitharevalla@users.noreply.github.com>
2020-02-22 14:52:00 -0800

    Update error.m

--- error.m
+++ error.m
@@ -1,4 +1,4 @@
-function e1 = error(A,k,U_k,S_k,V_k)
+function e1 = norm_error(A,k,U_k,S_k,V_k)
 [U1,S1,V1] = svds(A,k);
 ferr = @(u,v) norm(u(:),'fro')- norm(v(:),'fro');
  e1 = ferr(U1*S1*V1',(U_k*S_k*V_k));
